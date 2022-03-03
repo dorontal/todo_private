c0793ae42a94a7fedc6311bb79fdeff7f56a924e
Doron Tal <doron@tracktunes.org>
2022-03-03 18:48:20 -0500

    added

---
+++ android/app/src/main/kotlin/com/example/todo_private/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todo_private
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/services/amplify.dart
@@ -0,0 +1,63 @@
+import 'dart:developer' as developer;
+import 'dart:async';
+import 'package:amplify_storage_s3/amplify_storage_s3.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:amplify_api/amplify_api.dart';
+import '../models/ModelProvider.dart';
+import '../amplifyconfiguration.dart';
+
+/// Service for all things Amplify (Auth, API, Storage)
+class AmplifyService {
+  bool amplifyIsConfigured = false;
+
+  final StreamController<AuthUser?> userStreamController =
+      StreamController<AuthUser?>();
+
+  // returns whether or not Amplify is configured
+  Future<void> initUserStream() async {
+    developer.log('initUserStream()');
+    AuthUser? cognitoUser = await getCognitoUser();
+    userStreamController.add(cognitoUser);
+  }
+
+  Future<void> configureAmplify() async {
+    developer.log('configureAmplify()');
+    if (!amplifyIsConfigured) {
+      await Amplify.addPlugins([
+        AmplifyDataStore(modelProvider: ModelProvider.instance),
+        AmplifyAuthCognito(),
+        AmplifyAPI(modelProvider: ModelProvider.instance),
+        AmplifyStorageS3()
+      ]);
+      await Amplify.configure(amplifyconfig);
+      amplifyIsConfigured = true;
+    }
+  }
+
+  Future<AuthUser?> getCognitoUser() async {
+    //   // This version barfs if you delete the user and restart app
+    //   final result = await Amplify.Auth.fetchAuthSession();
+    //   if (result.isSignedIn == true) {
+    //     final AuthUser cognitoUser = await Amplify.Auth.getCurrentUser();
+    //     developer.log('*********** getCurrentUser() got one!');
+    //     developer.inspect(cognitoUser);
+    //     return cognitoUser;
+    //   } else {
+    //     developer.log('*********** getCurrentUser() - signed out');
+    //     return null;
+    //   }
+    // }
+    try {
+      final AuthUser cognitoUser = await Amplify.Auth.getCurrentUser();
+      developer.log('*********** getCurrentUser() got one!');
+      developer.inspect(cognitoUser);
+      return cognitoUser;
+      // } on SignedOutException catch (e) {
+    } on AuthException catch (e) {
+      developer.log('*********** getCurrentUser() - signed out - e: $e');
+      return null;
+    }
+  }
+}
