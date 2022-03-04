34e74e184bb25aef7eba9bed3919fe7c6bacb1a0
Doron Tal <doron@tracktunes.org>
2022-03-04 17:58:37 -0500

    Occam

--- lib/providers/amplify.dart
+++ lib/providers/amplify.dart
@@ -1,4 +1,3 @@
-import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import '../models/ModelProvider.dart';
@@ -6,11 +5,6 @@ import '../services/amplify.dart';
 
 final amplifyServiceProvider =
     Provider<AmplifyService>((_) => AmplifyService());
-
-final authUserStreamProvider = StreamProvider<AuthUser?>((ref) {
-  final amplifyService = ref.watch(amplifyServiceProvider);
-  return amplifyService.userStreamController.stream;
-});
 
 final todosSnapshotProvider = StreamProvider((ref) {
   return Amplify.DataStore.observeQuery(Todo.classType);
