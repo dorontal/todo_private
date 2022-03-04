807553861fa11c0cf65b1dee91d3ac657c314dec
Doron Tal <doron@tracktunes.org>
2022-03-04 16:39:13 -0500

    minor

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'constants/style.dart';
 import 'providers/amplify.dart';
-import 'screens/todo.dart';
+import 'screens/todos.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,15 +16,13 @@ class App extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final amplifyService = ref.watch(amplifyServiceProvider);
-    amplifyService
-        .configureAmplify()
-        .then((_) => amplifyService.startUserStream());
+    amplifyService.configureAmplify();
     return Authenticator(
       child: MaterialApp(
           title: 'Todo List',
           theme: appTheme,
           builder: Authenticator.builder(),
-          home: Todos()),
+          home: const Todos()),
     );
   }
 }
