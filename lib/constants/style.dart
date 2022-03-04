e09e80691132beb767b4d22f12158dee0828f12a
Doron Tal <doron@tracktunes.org>
2022-03-04 10:47:00 -0500

    minor

--- lib/constants/style.dart
+++ lib/constants/style.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
 
-// This file contains constants that affect the style of the app in
-// various widgets
+// Constants that affect the style of the app in various widgets
 
 final appTheme = ThemeData.dark();
 
-/// Screen width below which the screen is single column, rather than
-/// two-column, with a menu icon at the top left that opens up the
-/// drawer.
-const splitViewMaxWidth = 670;
-
-// const buttonColor = Color.fromRGBO(239, 68, 35, 1.0);
-// const buttonColor = Color(0xffef4423);
-
-const logoImageURL =
-    'https://wg9r5td5e.tracktunes.org/assets/image/tracktunes-logo.png';
+const todoItemIconSize = 24.0;
--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -1,36 +1,13 @@
-import 'dart:developer' as developer;
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todo_private/providers/amplify.dart';
 import '../models/ModelProvider.dart';
+import '../constants/style.dart';
 
 class TodoItem extends ConsumerWidget {
-  final double iconSize = 24.0;
   final Todo todo;
 
   const TodoItem({Key? key, required this.todo}) : super(key: key);
-
-  void _deleteTodo(BuildContext context) async {
-    try {
-      // to delete data from DataStore, we pass the model instance to
-      // Amplify.DataStore.delete()
-      await Amplify.DataStore.delete(todo);
-    } catch (e) {
-      developer.log('An error occurred while deleting Todo: $e');
-    }
-  }
-
-  Future<void> _toggledone() async {
-    // copy the Todo we wish to update, but with updated properties
-    Todo updatedTodo = todo.copyWith(done: todo.done);
-    try {
-      // to update data in DataStore, we again pass an instance of a model to
-      // Amplify.DataStore.save()
-      await Amplify.DataStore.save(updatedTodo);
-    } catch (e) {
-      developer.log('An error occurred while saving Todo: $e');
-    }
-  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -58,7 +35,7 @@ class TodoItem extends ConsumerWidget {
               ),
             ),
             Icon(todo.done! ? Icons.check_box : Icons.check_box_outline_blank,
-                size: iconSize),
+                size: todoItemIconSize),
           ]),
         ),
       ),
