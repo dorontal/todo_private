a74524de58243d891081c01c175bdd16a790ee95
Doron Tal <doron@tracktunes.org>
2022-03-04 18:11:47 -0500

    minor

---
+++ scripts/git_tag.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# tags current repo based on VERSION file in git repository's root directory
+
+# NOTES: 
+# 1) this script takes zero arguments. It uses VERSION. for the tag comment
+# 2) only increment version and CHANGELOG *after* running this but first,
+#    it's best to do a `git_save`
+# 3) Some useful commands:
+#    List tags                     git tag
+#    Delete local tag              git tag -d <tag_name>
+#    Push local deletion to cloud  git push --delete origin <tag_name>
+#    (see https://devconnected.com/how-to-delete-local-and-remote-tags-on-git/)
+
+MY_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cd $MY_DIR/..
+
+TAG="v`cat VERSION | perl -pe 's/^(.*)\.\d+$/$1/'`"
+
+git tag -a "$TAG" -m "git tag w/version: $TAG"
+
+git push origin "$TAG"
